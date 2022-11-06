ce941b8be5f4c65943a91c0b84f1ecf5c5e43702
abdelsurya <abdelsurya@gmail.com>
2022-11-06 17:11:11 +0700

    update

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".mouse-laymas").css("cursor", "pointer");
+});
+
+$(document).ready(function () {
+  $(".mouse-laymas").click(function () {
+    $("#myModal").modal("toggle");
+  });
+});

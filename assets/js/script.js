21906df4f6bed7fc4cd565d5dfe6ad0168bdaba7
abdelsurya <abdelsurya@gmail.com>
2022-11-07 23:45:07 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,46 @@ $(document).ready(function () {
     }
   );
 });
+
+$(document).ready(function () {
+  $(".slideclick1").css("cursor", "pointer");
+});
+$(document).ready(function () {
+  $(".slideclick1").click(function () {
+    $(".up1").toggleClass("bi bi-chevron-up");
+  });
+});
+
+$(document).ready(function () {
+  $(".slideclick2").css("cursor", "pointer");
+});
+$(document).ready(function () {
+  $(".slideclick2").click(function () {
+    $(".up2").toggleClass("bi bi-chevron-up");
+  });
+});
+
+$(document).ready(function () {
+  $(".slideclick3").css("cursor", "pointer");
+});
+$(document).ready(function () {
+  $(".slideclick3").click(function () {
+    $(".up3").toggleClass("bi bi-chevron-up");
+  });
+});
+
+$(document).ready(function () {
+  $(".headingjht").click(function () {
+    $(".collapsejht").slideToggle("slow");
+  });
+});
+$(document).ready(function () {
+  $(".headingjkm").click(function () {
+    $(".collapsejkm").slideToggle("slow");
+  });
+});
+$(document).ready(function () {
+  $(".headingjkk").click(function () {
+    $(".collapsejkk").slideToggle("slow");
+  });
+});

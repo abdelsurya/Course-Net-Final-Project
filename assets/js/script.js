a82ed7464797eac332f0078487a90e7aa2c8d906
abdelsurya <abdelsurya@gmail.com>
2022-12-12 21:52:14 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,7 @@ $(document).ready(function () {
 $(document).ready(function () {
   $(".slideclick2").css("cursor", "pointer");
 });
+
 $(document).ready(function () {
   $(".slideclick2").click(function () {
     $(".up2").toggleClass("bi bi-chevron-up");
@@ -29,6 +30,7 @@ $(document).ready(function () {
 $(document).ready(function () {
   $(".slideclick3").css("cursor", "pointer");
 });
+
 $(document).ready(function () {
   $(".slideclick3").click(function () {
     $(".up3").toggleClass("bi bi-chevron-up");
@@ -38,6 +40,7 @@ $(document).ready(function () {
 $(document).ready(function () {
   $(".slideclick4").css("cursor", "pointer");
 });
+
 $(document).ready(function () {
   $(".slideclick4").click(function () {
     $(".up4").toggleClass("bi bi-chevron-up");
@@ -49,21 +52,25 @@ $(document).ready(function () {
     $(".collapsejkk").slideToggle("slow");
   });
 });
+
 $(document).ready(function () {
   $(".headingjkm").click(function () {
     $(".collapsejkm").slideToggle("slow");
   });
 });
+
 $(document).ready(function () {
   $(".headingjht").click(function () {
     $(".collapsejht").slideToggle("slow");
   });
 });
+
 $(document).ready(function () {
   $(".headingjpn").click(function () {
     $(".collapsejpn").slideToggle("slow");
   });
 });
+
 $(document).ready(function () {
   $(".headingsimulasi").click(function () {
     $(".collapsesimulasi").slideToggle("slow");
